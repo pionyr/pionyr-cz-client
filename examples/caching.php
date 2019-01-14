e69bb54ab38314ef023c5d7d1635acdabc244307
Ondřej Machulda <ondrej.machulda@gmail.com>
2019-01-14 12:23:05 +0100

    Add caching example

---
+++ examples/caching.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+/**
+ * Pro nastavení cachování odpovědí API je třeba nainstalovat cache-plugin:
+ * `composer require php-http/cache-plugin`
+ * Dále je třeba cachovací adaptér (implementace CacheItemPoolInterface z PSR-6) - například FilesystemAdapter ze Symfony/Cache
+ * `composer require symfony/cache`
+ *
+ * Pak už stačí jenom zabalit instanci `HttpClientDiscovery` do  instance`PluginClient` - viz příklad níže.
+ * Pro správné fungování cachování je také nastavit ignorování cache hlaviček serveru.
+ *
+ * @see http://docs.php-http.org/en/latest/plugins/cache.html
+ */
+use Http\Client\Common\Plugin\CachePlugin;
+use Http\Client\Common\PluginClient;
+use Http\Discovery\HttpClientDiscovery;
+use Http\Discovery\StreamFactoryDiscovery;
+use Pionyr\PionyrCz\PionyrCz;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$pionyrCz = new PionyrCz('...');
+
+$cachePlugin = new CachePlugin(
+    new FilesystemAdapter('', 0, __DIR__ . '/cache/'), // cache bude ukládána do složky cache/
+    StreamFactoryDiscovery::find(),
+    [
+        'respect_response_cache_directives' => [], // pro ignorování no-cache hlaviček v odpovědích API
+        'default_ttl' => null, // pro ignorování nesprávných údajů v hlavičkách odpovědí API
+        'cache_lifetime' => 3600, // lifetime cache v sekundách - může být i více, ale pak je vhodné si udělat invalidaci (viz příklad níže)
+    ]
+);
+
+$httpClient = new PluginClient(HttpClientDiscovery::find(), [$cachePlugin]);
+$pionyrCz->setHttpClient($httpClient);
+
+// Všechny request provedené přes $pionyrCz budou nyní používat cache s platností 1 hodina
+$response = $pionyrCz
+    ->request()
+    ->articles()
+    ->send();
+
+// Invalidace cache - smaže všechna nacachovaná data
+$cacheAdapter = new FilesystemAdapter('', 0, __DIR__ . '/cache/');
+$cacheAdapter->clear();
